4611a15eb5208296d52ab8fdb9ff8f09a3499651
ratsharadich <zharchenko.aleksander@gmail.com>
2022-11-04 16:12:14 +0800

    chore: installed tailwind and eslint

---
+++ src/pages/Orders/index.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import { Background } from "src/shared";
+
+export const Orders: FC = () => {
+  return <><Background /></>;
+};
---
+++ src/shared/Background/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const Background: FC = () => {
+  return (
+    <div className="h-screen w-screen">
+      <div className="absolute w-[28.75rem] h-[43.25rem] left-[61rem] top-[79.25rem]"></div>
+    </div>
+  );
+};
